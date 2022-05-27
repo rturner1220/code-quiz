07c620267659ad718e914fc2ae1876af8ee3632a
Rosa Turner <rmturner1220@gmail.com>
2022-05-27 12:54:16 -0400

    set questions and answers

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,20 +1,98 @@
 // first, starts to give function to the quiz-button
-var startButton = document.getElementById('start-btn')
+var startButton = document.getElementById('quiz-btn')
+var nextButton = document.getElementById('continue-btn')
+var mainContentElement = document.getElementById('main-content')
+var questionElement  = document.getElementById('question')
+var answerButtons = document.getElementById('answer-btn')
+
+var throwQuestion, questionIndex;
+
+
 startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
-console.log('started')
-
+startButton.classList.add('hide')
+startButton.classList.add("hide-away");
+throwQuestion = myQuestions.sort(() => Math.random() - .5)
+questionIndex = 0
+mainContentElement.classList.remove('hide')
+setQuestions()
 }
 
 // Second, set the questions
-function SetQuestions() {
-
-
+function setQuestions() {
+resetState()
+showQuestion(throwQuestion[questionIndex])
+}
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+      var button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+          button.dataset.correct = answer.correct
+      }
+    button.addEventListener('click', selectAnswer) 
+    answerButtons.appendChild(button)
+    })
 }
 
-// Third, select answers
-function SelectQuestion() {
-
+function resetState() {
+ nextButton.classList.add('hide')
+ while (answerButtons.firstChild) {
+ answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+function selectAnswer(e) {
     
 }
+
+var myQuestions = [
+        {      
+        question: 'How can a datatype be declared to be a constant type?',
+        answers: [    
+         { text: 'const', correct: true },
+         { text: 'var', correct: false },
+         { text: 'let', correct: false },
+         { text: 'constant', correct: false }
+         ] 
+        },
+        {
+        question: 'Which of the following methods can be used to display data in some form using JavaScript?',
+        answers: [    
+        { text: 'document. Write ()', correct: false },
+        { text: 'console.log()', correct: false },
+        { text: 'window.Alert', correct: false },
+        { text: 'All the above', correct: true }
+             ]   
+        },
+        {
+        question: 'Which of the following keywords is used to define a variable in JavaScript?',
+        answers: [    
+        { text: 'var', correct: false },
+        { text: 'let', correct: false },
+        { text: 'Both A and B', correct: true },
+        { text: 'constant', correct: false }
+                 ]   
+        },
+        {
+        question: 'Which of the following are closures in JavaScript?',
+        answers: [    
+        { text: 'Variables', correct: false },
+        { text: 'Functions', correct: false },
+        { text: 'Objects', correct: false },
+        { text: 'All the above', correct: true }
+                 ]   
+        },
+        {
+        question: 'How do we write a comment in JavaScript?',
+        answers: [    
+        { text: '/* */', correct: false },
+        { text: '//', correct: true },
+        { text: '#', correct: false },
+        { text: '$ $', correct: false }
+               ]   
+        },
+    ]
+       
